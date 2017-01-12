39f900d646fe218e9180e6a6f6ffc02636555efa
Morgan Nomura <morganelle@gmail.com>
2017-01-11 18:59:44 -0800

    app.js is now collecting form data and adding it to the chart

--- app.js
+++ app.js
@@ -22,6 +22,23 @@ Store.prototype.calcCookieEst = function() {
     this.dayTotal += result;
   }
 };
+Store.prototype.buildRow = function() {
+  this.row = this.hourlyCookies;
+  this.row.push(this.dayTotal);
+  this.row.unshift(this.location);
+  return this.row;
+};
+Store.prototype.render = function() {
+  this.calcCookieEst();
+  this.buildRow();
+  var rowEl = document.createElement('tr');
+  for (var i = 0; i < this.row.length; i++) {
+    var tableDataEl = document.createElement('td');
+    tableDataEl.textContent = this.row[i];
+    rowEl.appendChild(tableDataEl);
+  }
+  tableEl.appendChild(rowEl);
+};
 
 // Construct new object
 var firstAndPike = new Store('1st and Pike', 6, 21, 23, 65, 6.3);
@@ -40,90 +57,65 @@ var stores = [firstAndPike, seaTac, seattleCenter, capHill, alki];
 var tableEl = document.getElementById('sales');
 
 // Populates table in HTML
-var populateTable = function(stores) {
-  // steps through stores array, creates a row, and adds a store name td, has a nested loop to add all items in hourlyCookies array
-  for (var i = 0; i < stores.length; i++) {
-    console.log('outer for loop begins ' + i);
-    // var tableEl = document.getElementById('sales');
-    var rowEl = document.createElement('tr');
-    var store = stores[i];
-    // Creates store name td at beginning of each tr
-    var tableData = document.createElement('td');
-    tableData.setAttribute('class','store-name');
-    tableData.textContent = store.location;
-    rowEl.appendChild(tableData);
-    // runs calcCookieEst to get array of hourly estimates by store
-    store.calcCookieEst();
-    // steps through each array of store.hourlyCookies values and populates a td
-    for (var j = 0; j <= store.hourlyCookies.length; j++) {
-      var tableContent = store.hourlyCookies[j];
-      var tableData = document.createElement('td');
-      tableData.textContent = tableContent;
-      rowEl.appendChild(tableData);
-    }
-    // add daily store estimates as last td
-    tableData.textContent = store.dayTotal;
-    rowEl.appendChild(tableData);
-    // attaches the table row to DOM
-    tableEl.appendChild(rowEl);
+var populateRow = function(store) {
+  store.calcCookieEst();
+  store.buildRow();
+  var rowEl = document.createElement('tr');
+  for (var i = 0; i < store.row.length; i++) {
+    var tableDataEl = document.createElement('td');
+    tableDataEl.textContent = store.row[i];
+    rowEl.appendChild(tableDataEl);
   }
+  tableEl.appendChild(rowEl);
 };
 
-// Populates footer table row with hourly totals
-var footer = function(store) {
-  var rowFooterEl = document.createElement('tr');
-  var tableFooterData = document.createElement('td');
-  tableFooterData.setAttribute('class','store-name');
-  tableFooterData.textContent = 'Hourly totals';
-  rowFooterEl.appendChild(tableFooterData);
-  // For loop to add hourly totals across all stores
-  for (var k = 0; k < firstAndPike.hourlyCookies.length; k++) {
-    var tableFooterCount = document.createElement('td');
-    tableFooterCount.textContent = firstAndPike.hourlyCookies[k] + seaTac.hourlyCookies[k] + seattleCenter.hourlyCookies[k] + capHill.hourlyCookies[k] + alki.hourlyCookies[k];
-    rowFooterEl.appendChild(tableFooterCount);
-  }
-  // adds empty field as last td
-  var tableFooterBlank = document.createElement('td');
-  tableFooterBlank.textContent = ' ';
-  rowFooterEl.appendChild(tableFooterBlank);
-  // attaches the table row to DOM
-  tableEl.appendChild(rowFooterEl);
-};
+firstAndPike.render();
+seaTac.render();
+seattleCenter.render();
+capHill.render();
+alki.render();
 
-// var addStoreFormEl = document.getElementById('add-store-form'); // declares var from HTML form id
-//
-// // register submit event from addStoreFormEl
-// addStoreFormEl.addEventListener('submit', function(event) {
-//   event.preventDefault();
-//   event.stopPropagation();
-//
-//   console.log(event.target.storename.value);
-//   console.log(event.target.storeopen.value);
-//   console.log(event.target.storeclose.value);
-//   console.log(event.target.mincust.value);
-//   console.log(event.target.maxcust.value);
-//   console.log(event.target.avgcookies.value);
-//
-// }, false);
+// // Populates footer table row with hourly totals
+// var footer = function(store) {
+//   var rowFooterEl = document.createElement('tr');
+//   var tableFooterData = document.createElement('td');
+//   tableFooterData.setAttribute('class','store-name');
+//   tableFooterData.textContent = 'Hourly totals';
+//   rowFooterEl.appendChild(tableFooterData);
+//   // For loop to add hourly totals across all stores
+//   for (var k = 0; k < firstAndPike.hourlyCookies.length; k++) {
+//     var tableFooterCount = document.createElement('td');
+//     tableFooterCount.textContent = firstAndPike.hourlyCookies[k] + seaTac.hourlyCookies[k] + seattleCenter.hourlyCookies[k] + capHill.hourlyCookies[k] + alki.hourlyCookies[k];
+//     rowFooterEl.appendChild(tableFooterCount);
+//   }
+//   // adds empty field as last td
+//   var tableFooterBlank = document.createElement('td');
+//   tableFooterBlank.textContent = ' ';
+//   rowFooterEl.appendChild(tableFooterBlank);
+//   // attaches the table row to DOM
+//   tableEl.appendChild(rowFooterEl);
+// };
 
 var addStoreFormEl = document.getElementById('add-store-form'); // declares var from HTML form id
 
 // register submit event on form
-addStoreFormEl.addEventListener('submit', function(event){
+addStoreFormEl.addEventListener('submit', function() {submitEvent(event);}, false); // false included for old browsers; may be redundant if using stopPropagation
+
+// Event handler, a la arthur
+var submitEvent = function(event) {
   event.preventDefault(); // may be default to load page
   event.stopPropagation(); // if not added, could fire event to any ancestor element
+  var ename = (event.target.storename.value);
+  var eopen = Number.parseInt(event.target.storeopen.value);
+  var eclose = Number.parseInt(event.target.storeclose.value);
+  var emin = Number.parseInt(event.target.mincust.value);
+  var emax = Number.parseInt(event.target.maxcust.value);
+  var eavg = Number.parseInt(event.target.avgcookies.value);
 
-  // target = what event listener was applied to
-  // console.log(event.target);
-  // .value gets info between attributes
-  console.log(event.target.storename.value);
-  console.log(event.target.storeopen.value);
-  console.log(event.target.storeclose.value);
-  console.log(event.target.mincust.value);
-  console.log(event.target.maxcust.value);
-  console.log(event.target.avgcookies.value);
+  ename = new Store(ename, eopen, eclose, emin, emax, eavg);
+  console.log(ename);
+  ename.render();
+  console.log(ename.hourlyCookies);
+};
 
-}, false); // false included for old browsers; may be redundant if using stopPropagation
-
-populateTable(stores);
-footer();
+// footer();
